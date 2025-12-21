86e4e66ad0be733bfb8a58cb2735020cf92d649e
lajesfen <luciano.aguirre@utec.edu.pe>
2025-12-21 11:42:22 -0500

    add: clickToMove camera controller

--- src/components/CameraController.tsx
+++ src/components/CameraController.tsx
@@ -1,27 +1,36 @@
 import { PerspectiveCamera } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
+import { useRef } from "react";
 import * as THREE from "three";
 
-function CameraController() {
+interface Props {
+  targetPosition: THREE.Vector3;
+  targetLookAt: THREE.Vector3;
+}
+
+function CameraController({ targetPosition, targetLookAt }: Props) {
   const { camera } = useThree();
-  const initialPosition = new THREE.Vector3(-5, 8, 15.2);
-  const targetPosition = new THREE.Vector3();
+
+  const internalTarget = useRef(new THREE.Vector3(-5, 8, 15.2));
+  const internalLookAt = useRef(new THREE.Vector3(0, 1.5, 0));
 
   useFrame((state) => {
-    const offsetX = state.pointer.x * 0.5;
-    const offsetY = state.pointer.y * -0.5;
+    const offsetX = state.pointer.x * 1.5;
+    const offsetY = state.pointer.y * -1.5;
 
-    targetPosition.set(
-      initialPosition.x + offsetX,
-      initialPosition.y + offsetY,
-      initialPosition.z
+    internalTarget.current.set(
+      targetPosition.x + offsetX,
+      targetPosition.y + offsetY,
+      targetPosition.z
     );
 
-    camera.position.lerp(targetPosition, 0.05);
-    camera.lookAt(0, 1.5, 0);
+    camera.position.lerp(internalTarget.current, 0.05);
+
+    internalLookAt.current.lerp(targetLookAt, 0.04);
+    camera.lookAt(internalLookAt.current);
   });
 
-  return <PerspectiveCamera makeDefault position={initialPosition} fov={32} />;
+  return <PerspectiveCamera makeDefault fov={32} position={[-5, 8, 15.2]} />;
 }
 
 export default CameraController;
