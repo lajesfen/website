07b29f2bb39abafe1debf01ce4cba3196e203407
lajesfen <luciano.aguirre@utec.edu.pe>
2026-01-24 12:41:26 -0500

    feat: date month + year display for projects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,12 +1,16 @@
+import { formatDate } from "../utils/date";
+
 export interface ProjectProps {
   title: string;
   description: string;
-  imageUrl: string;
-  date: string;
+  imageUrl?: string;
+  date: Date;
   url?: string;
 }
 
 function Project({ title, description, imageUrl, url, date }: ProjectProps) {
+  const stringDate = formatDate(date);
+
   return (
     <div
       className="flex flex-row gap-3 p-1 items-center hover:opacity-60 cursor-pointer transition-opacity duration-200"
@@ -14,7 +18,7 @@ function Project({ title, description, imageUrl, url, date }: ProjectProps) {
     >
       <img
         src={imageUrl}
-        className="aspect-square rounded-lg outline-2 outline-white drop-shadow-lg -rotate-8"
+        className="aspect-square rounded-sm sm:rounded-md md:rounded-lg outline-2 outline-white drop-shadow-lg -rotate-8"
         width={36}
         height={36}
       />
@@ -22,7 +26,7 @@ function Project({ title, description, imageUrl, url, date }: ProjectProps) {
         <p>{title}</p>
         <p className="text-sm text-[#A0A0A0] -mt-0.5">{description}</p>
       </div>
-      <p className="text-sm text-right whitespace-nowrap">{date}</p>
+      <p className="text-sm text-right whitespace-nowrap">{stringDate}</p>
     </div>
   );
 }
