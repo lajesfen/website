c3b0745e4cf3c43544345834948a65a6741a23c8
lajesfen <luciano.aguirre@utec.edu.pe>
2026-07-19 17:37:38 -0500

    chore: add infinite-minesweeper project

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,8 +6,8 @@ interface LayoutProps {
 
 export const Layout = ({ children }: LayoutProps) => (
   <main className="w-full min-h-screen flex flex-col items-center justify-start">
-    <div className="fixed top-0 left-0 w-full h-[12vh] bg-linear-to-b from-background to-background/0 z-10 pointer-events-none" />
-    <div className="fixed bottom-0 left-0 w-full h-[12vh] bg-linear-to-t from-background to-background/0 z-10 pointer-events-none" />
+    <div className="fixed top-0 left-0 w-full h-[10vh] bg-linear-to-b from-background to-background/0 z-10 pointer-events-none" />
+    <div className="fixed bottom-0 left-0 w-full h-[10vh] bg-linear-to-t from-background to-background/0 z-10 pointer-events-none" />
     <Toolbar />
     {children}
   </main>
